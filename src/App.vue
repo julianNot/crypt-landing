1dc4fbdfd099b217386e9fda179d3f6c0b795f4d
Julian Ardila <julian.ardila@uptc.edu.co>
2022-03-29 18:45:10 -0500

    create Table, with crypts by Coingecko API

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,30 @@
       classImg="header-icon"
     />
     <div class="header-title-container">
-      <Paragraph text="La próxima revolución en el intercambio de criptomonedas." classSize="size-one-24rem" class="h1" />
+      <Paragraph text="La próxima revolución en el intercambio de criptomonedas." classSize="size-one-24rem" class="h2" />
       <Paragraph text="Patacoin te ayuda a navegar entre los diferentes precios y tendencias." classSize="size-two-14rem" class="p" />
+      <a href="#plans" class="header--button">Conoce Nuestros Planes <span class="arrow-button"></span></a>
     </div>
   </header>
+  <main>
+    <section class="main-exchange-container">
+      <div class="background-img">
+      </div>
+      <div class="main-exchange-container--title">
+        <Paragraph text="Visibilizamos todas las tasas de cambio." classSize="size-one-24rem" class="h2"/>
+        <Paragraph text="Traemos información en tiempo real de las casas de cambio y las monedas más importantes del mundo." classSize="size-two-14rem" class="p"/>
+      </div>
+      <Table />
+    </section>
+  </main>
+  
 </template>
 
 <script>
 /* Components */
 import Icon from "@/components/Icon.vue";
 import Paragraph from '@/components/Paragraph.vue'
+import Table from '@/components/Table.vue'
 
 /* Img and Icons */
 import headerIcon from "@/assets/images/logo.svg";
@@ -33,6 +47,7 @@ export default {
   components: {
     Icon,
     Paragraph,
+    Table,
   },
 };
 </script>
@@ -46,6 +61,8 @@ export default {
   --blue-green: #196774;
   --black-solf: #363432;
   --just-white : #ffffff;
+  --soft-orange: #ffe9d5;
+  --off-white: #faf8f7;
 }
 
 * {
@@ -57,6 +74,7 @@ export default {
 html {
   font-size: 62.5%;
   font-family: "DM Sans", sans-serif;
+  /* background-color: var(--off-white); */
 }
 
 header {
@@ -68,8 +86,7 @@ header {
   min-width: 320px;
   height: 334px;
   text-align: center;
-  background: rgb(54,52,50);
-  background: linear-gradient(90deg, var(--black-solf) 16%, var(--orange-hard) 100%);
+  background: linear-gradient(207.8deg, #201e1c 16.69%, #f7931a 100%);
 }
 
 .header-title-container{
@@ -88,10 +105,94 @@ header {
   color: var(--just-white);
 }
 
+.h2{
+  font-weight: bold;
+  line-height: 2.6rem;
+  color: var(--black-solf);
+}
+
 .header-title-container p{
   margin-top: 25px;
   line-height: 1.8rem;
   font-weight: 500;
-  color: var(--orange-soft);
+  color: var(--soft-orange);
+}
+
+.header-title-container .header--button{
+  position: absolute;
+  left: calc(50% - 114.5px);
+  top: 270px;
+  display: block;
+  margin-top: 35px;
+  padding: 15px;
+  width: 229px;
+  height: 48px;
+  background-color: var(--just-white);
+  box-shadow: 0px 5px 10px rgba(89, 73, 39, 0.2);
+  border: none;
+  border-radius: 10px;
+  font-size: 1.4rem;
+  font-weight: bold;
+  text-decoration: none;
+  color: var(--black-solf);
+}
+
+.header-title-container .header--button span{
+  display: inline-block;
+  width: 13px;
+  height: 8px;
+  margin-left: 8px;
+  background-image: url(./assets/icons/Vector.svg);
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+main{
+  width: 100%;
+  height: auto;
+  background-color: var(--off-white);
+}
+
+.main-exchange-container{
+  width: 90%;
+  min-width: 288px;
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+.main-exchange-container--title {
+  width: 90%;
+  min-width: 288px;
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+.main-exchange-container .background-img{
+  width: 200px;
+  height: 200px;
+  margin: 0 auto;
+  margin-bottom: 50px;
+  background-image: url(./assets/images/bitcoin.svg);
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+}
+
+.main-exchange-container .h2{
+  text-align: center;
+  margin-bottom: 30px;
+  font-weight: bold;
+  line-height: 2.6rem;
+  color: var(--black-solf);
+}
+
+.main-exchange-container .p{
+  margin-bottom: 30px;
+  font-size: 1.4rem;
+  font-weight: 500;
+  line-height: 1.6rem;
+  text-align: center;
+  color: #757575;
 }
 </style>
---
+++ src/components/Table.vue
@@ -0,0 +1,135 @@
+<template>
+    <section class="main-table-container">
+        <div class="main-currency-table">
+            <p class="currency-table--title">Monedas</p>
+            <div class="currency-table--container">
+                <table>
+                    <tr>
+                        <td class="table__top-left">name</td>
+                        <td class="table__top-right table__right">price</td>
+                    </tr>
+                    <tr>
+                        <td>Name1</td>
+                        <td class="table__right">price</td>
+                    </tr>
+                    <tr>
+                        <td>Name2</td>
+                        <td class="table__right">price</td>
+                    </tr>
+                    <tr>
+                        <td class="table__bottom-left">Name3</td>
+                        <td class="table__bottom-right table__right">Price</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="currency-table--date">
+                <p><b>Actualizado:</b> Fecha</p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            crypts : [],
+            endList : Number
+        }
+    },
+    methods : {
+        async getCrypts(){
+            await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=4&page=1&sparkline=false').then(resp => resp.json())
+            .then(data => console.log(data)/* {
+                const {crypt1, crypt2, crypt3, crypt4} = data
+                console.log(data);
+                this.crypts.push(crypt1)
+                this.crypts.push(crypt2)
+                this.crypts.push(crypt3)
+                this.crypts.push(crypt4)
+            } */)
+        },
+        mounted() {
+            return this.getCrypts()
+        },
+    },
+}
+</script>
+
+<style>
+:root{
+    --soft-orange: #ffe9d5;
+}
+
+.main-currency-table{
+    width: 70%;
+    min-width: 235px;
+    max-width: 500px;
+    height: 360px;
+    margin: 0 auto;
+    text-align: center;
+}
+
+.main-currency-table .currency-table--title{
+    margin-bottom: 15px;
+    font-size: 1.8rem;
+    line-height: 2.3rem;
+    font-weight: 700;
+    color: #ff9536;
+}
+
+.currency-table--container {
+    width: 90%;
+    min-width: 230px;
+    max-width: 300px;
+    height: 250px;
+    margin: 0 auto;
+}
+
+.currency-table--container table{
+    width: 100%;
+    height: 100%;
+}
+
+.currency-table--container td {
+    width: 50%;
+    font-size: 1.6rem;
+    font-weight: 500;
+    line-height: 1.9rem;
+    color: #90a19d;
+    background-color: #ffffff;
+}
+
+.currency-table--container .table__top-left{
+    border-radius: 15px 0 0 0;
+}
+
+.currency-table--container .table__top-right{
+    border-radius: 0 15px 0 0;
+}
+
+.currency-table--container .table__bottom-left {
+  border-radius: 0 0 0 15px;
+}
+
+.currency-table--container .table__bottom-right {
+  border-radius: 0 0 15px 0;
+}
+
+.currency-table--container .table__right{
+    font-size: 1.4rem;
+    font-weight: normal;
+    line-height: 1.7rem;
+    color: #757575;
+}
+
+.currency-table--date{
+    width: 190px;
+    height: 30px;
+    margin: 0 auto;
+    margin-top: 15px;
+    padding: 8px;
+    background-color: var(--soft-orange);
+    border-radius: 8px;
+}
+</style>
